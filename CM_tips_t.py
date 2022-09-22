53307afd5c7e2465298087671510ec03e782204d
JAAT JI <112750226+Jaatbro23@users.noreply.github.com>
2022-09-22 18:55:17 +0530

    Add files via upload

---
+++ CM_tips_t.py
@@ -0,0 +1,34 @@
+#Copyright ©️ 2021 CM TiPs. All Rights Reserved
+#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
+# ##Credits - [Countdown Timer Telegram bot by CM TiPs] (https://t.me/Tips_by_CM
+
+# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://t.me/Tips_by_CM
+                       
+  
+START_TEXT = 'Hello,\nMy name is **Countdown Timer Bot By CM** ⏰\n\nI can countdown to your important events in **any group chat**. 🥷\nUse me to keep track of how much time is left for the event ⏳\n\nHit **❓HELP** to find out more about how to use me.'
+
+GROUP_TEXT="""
+👥 | **HELP & SUPPORT**
+<i>Feel free to report any issues regarding this service.</i>
+"""
+
+HELP_TEXT = """
+ | **GET STARTED**
+<i>Add your bot to the group.</i>
+👮🏻‍♂️ | **ADMIN PERMISSIONS**
+<i>Don't forget to make your bot admin of the group.</i>
+🕹 | **COMMANDS**
+✦ **set**
+<i>Send the command below in correct format to the group.</i>
+• <code>/set seconds "event"</code>
+**example**
+• <code>/set 10 "10 seconds countdown"</code>
+✦ **stopc**
+<i>You can use <code>/stopc</code> command to stop all countdowns.</i> 
+⚠️ | **WARNING**
+<i>Do not do more than 2 countdowns at the same time using the same bot.</i>
+"""
+
+TUTORIAL_TEXT = "💡 | Click the **🎥 Video JAAT JI (Telegram available Only)** button to see the way of creating this bot for your group easily(unavailable)CM."
+
+#Copyright ©️ 2021 CM TiPs. All Rights Reserved.
